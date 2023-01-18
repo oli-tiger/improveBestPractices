1ee888892153f174117f10c0b54a58dcee784dbb
aaron-olivieri-loepfe <aaron.olivieri@vandewiele.com>
2023-01-18 11:57:56 +0100

    further Exercise (creating classes and unittests)

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products carried in inventory
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("Product instance created");
+        }
+
+        public Product(string productName, string description, int productId) :this()
+        {
+            this.ProductName = productName;
+            this.ProductId = productId;
+            this.Description = description;           
+                     
+            Console.WriteLine("Product instance has a name: " + ProductName);
+        }
+
+        private string productName;
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        private int productId;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public string SayHello()
+        {
+            return "Hello " + ProductName + " (" + ProductId + "): " + Description;
+        }
+    }
+}
